5ce7e497a35c5380565b3a45e477e7acdbf4e087
Kavya-natta <151709486+Kavya-natta@users.noreply.github.com>
2024-11-07 11:16:17 +0530

    SuperDemo4.java

---
+++ SuperDemo4.java
@@ -0,0 +1,32 @@
+import java.io.*;
+class Point2D
+{
+	int x=0;
+	int y=0;
+	Point2D(int x ,int y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+}
+class Point3D extends Point2D
+{
+	int z;
+	Point3D()
+	{
+		super(0,0);
+		z=0;
+	}
+	Point3D(int x,int y,int z)
+	{
+		super(x,y);
+		this.z=z;
+	}
+}
+class Main
+{
+	public static void main(String args[])
+	{
+		Point3D ob = new Point3D();
+	}
+}
